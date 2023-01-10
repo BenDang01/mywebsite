2b94c889b39193a9e29150ebccdcbe6e174601f5
BenDang01 <86766792+BenDang01@users.noreply.github.com>
2023-01-11 01:45:29 +0700

    Add files via upload

---
+++ js/components.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function Project(props) {
+  return React.createElement("figure", null, React.createElement("a", {
+    href: props.url,
+    target: "_blank"
+  }, React.createElement("img", {
+    src: props.img
+  })), React.createElement("figcaption", null, React.createElement("a", {
+    href: props.url,
+    target: "_blank"
+  }, props.caption)));
+}
+
+function ProjectList() {
+  return React.createElement("div", {
+    id: "img-container"
+  }, React.createElement(Project, {
+    img: "file:///Users/ben/Desktop/Website/image/Youtube.png",
+    caption: "Youtube",
+    url: "https://www.youtube.com/@businessgame/featured"
+  }), React.createElement(Project, {
+    img: "file:///Users/ben/Desktop/Website/image/Avatar Builder.png",
+    caption: "Unseen World",
+    url: "https://unseenworld.substack.com/"
+  }), React.createElement(Project, {
+    img: "file:///Users/ben/Desktop/Website/image/ReactionTest_IMG.PNG",
+    caption: "Coming Soon",
+    url: "..."
+  }));
+}
+
+var el = React.createElement(ProjectList, null);
+ReactDOM.render(el, document.getElementById("img-container"));
