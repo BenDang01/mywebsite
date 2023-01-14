8873e43a23c7ab6e84e1da7d3c84c94627087899
BenDang01 <86766792+BenDang01@users.noreply.github.com>
2023-01-14 14:51:03 +0700

    Update components.js

--- js/components.js
+++ js/components.js
@@ -16,11 +16,11 @@ function ProjectList() {
   return React.createElement("div", {
     id: "img-container"
   }, React.createElement(Project, {
-    img: "../image/Youtube.png",
+    img: "image/Youtube.png",
     caption: "Youtube",
     url: "https://www.youtube.com/@businessgame/featured"
   }), React.createElement(Project, {
-    img: "../image/Avatar builder.png",
+    img: "image/Avatar builder.png"
     caption: "Unseen World",
     url: "https://unseenworld.substack.com/"
   }));
